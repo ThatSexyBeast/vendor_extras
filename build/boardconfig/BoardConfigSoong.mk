dfb71f505e589a9e2c6e2417da377eb7dbecc193
neporood <neporood@gmail.com>
2020-09-17 08:11:49 -0500

    soong: fix up Scorpion paths

--- build/boardconfig/BoardConfigSoong.mk
+++ build/boardconfig/BoardConfigSoong.mk
@@ -7,13 +7,13 @@ EXPORT_TO_SOONG := \
     TARGET_KERNEL_CONFIG \
     TARGET_KERNEL_SOURCE
 
-SOONG_CONFIG_NAMESPACES += dirtyVarsPlugin
+SOONG_CONFIG_NAMESPACES += scorpionVarsPlugin
 
-SOONG_CONFIG_dirtyVarsPlugin :=
+SOONG_CONFIG_scorpionVarsPlugin :=
 
 define addVar
-  SOONG_CONFIG_dirtyVarsPlugin += $(1)
-  SOONG_CONFIG_dirtyVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_scorpionVarsPlugin += $(1)
+  SOONG_CONFIG_scorpionVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
