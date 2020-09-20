3fe6655b7cf7bc2d37f517ce4c4a689efd94f64e
neporood <neporood@gmail.com>
2020-09-20 03:53:05 -0500

    soong: fixup Scorpion paths

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func dirtyExpandVariables(ctx android.ModuleContext, in string) string {
-	dirtyVars := ctx.Config().VendorConfig("dirtyVarsPlugin")
+func scorpionExpandVariables(ctx android.ModuleContext, in string) string {
+	scorpionVars := ctx.Config().VendorConfig("scorpionVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if dirtyVars.IsSet(name) {
-			return dirtyVars.String(name), nil
+		if scorpionVars.IsSet(name) {
+			return scorpionVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
